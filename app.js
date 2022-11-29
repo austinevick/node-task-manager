a5eb440f82df8aed1b1e8beec1d26690e86c37ca
Victor Augustine <augustinevickky@gmail.com>
2022-11-29 13:25:04 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/api/v1/tasks',router)
 app.use(errorHandler)
 
 
-const port = 3000;
+const port = process.env.PORT|| 3000;
 const start = async ()=>{
     try {
         await db(process.env.MONGO_URL);
